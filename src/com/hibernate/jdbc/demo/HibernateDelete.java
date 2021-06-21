3c7d4b387d12b37af95ae80b62a58516e652f7fa
arijitroy1 <connect.aroy1@yahoo.co.in>
2021-06-21 01:06:26 -0500

    Deliver for the first time

---
+++ src/com/hibernate/jdbc/demo/HibernateDelete.java
@@ -0,0 +1,42 @@
+package com.hibernate.jdbc.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.jdbc.model.Instructor;
+import com.hibernate.jdbc.model.InstructorDetail;
+
+public class HibernateDelete {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		Session session = null;
+		try {
+			session = factory.openSession();
+			session.beginTransaction();
+		
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, "2");
+			
+			System.out.println("Instructor Detail - "+instructorDetail);
+			
+			System.out.println("Associated Instructor - "+instructorDetail.getInstructor());
+			
+			session.delete(instructorDetail);//Will delete both
+			
+			session.getTransaction().commit();
+			
+		}catch(Exception e) {
+			System.out.print("Error - "+ e.getMessage());
+		}finally {
+			session.close();//If we get some exception and not able to close the session, here we will do that 
+			factory.close();
+		}
+	}
+
+}
